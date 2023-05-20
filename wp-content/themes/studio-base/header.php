c6f319aeb45c713d60354016ee9b6d797d4dada1
yamamura <ayatsujisan2@gmail.com>
2023-05-20 11:33:10 +0900

    コミットします

--- wp-content/themes/studio-base/header.php
+++ wp-content/themes/studio-base/header.php
@@ -14,12 +14,37 @@
            <div>
                <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/onigiri_tenmusu.png" alt="ロゴ" class="logo-img"></a>
            </div>
-           <ul class="inner">
-               <li class="inner_child"><a href="#">トップページ</a></li>
-               <li class="inner_child"><a href="#">1ページ目</a></li>
-               <li class="inner_child"><a href="#">2ページ目</a></li>
-               <li class="inner_child"><a href="#">3ページ目</a></li>
-               <li class="inner_child"><a href="#">お問い合わせページ</a></li>
-           </ul>
+           <div class="inner-area">
+                    <div class="header-tell">
+                        <p class="tell">
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/tell-icon.png" alt="電話アイコン" class="tell-icon">
+                            <span>0120-123-456</span>
+                        </p>
+                    </div>
+                        <ul class="inner">
+                            <li class="inner_child"><a href="#">トップページ</a></li>
+                            <li class="inner_child"><a href="#">1ページ目</a></li>
+                            <li class="inner_child"><a href="#">2ページ目</a></li>
+                            <li class="inner_child"><a href="#">3ページ目</a></li>
+                            <li class="inner_child"><a href="#">ニュースページ</a></li>
+                        </ul>
+                        <div class="sns-area">
+                            <ul class="sns-list">
+                                <li><a href=""><img src="<?php echo get_template_directory_uri(); ?>/images/insta.png" alt="インスタアイコン" class="insta-icon"></a></li>
+                                <li><a href=""><img src="<?php echo get_template_directory_uri(); ?>/images/twitter.png" alt="twitterアイコン" class="twitter-icon"></a></li>
+                            </ul>
+                        </div>
+            </div>
+            <div class="contact-area">
+                <a href="#" class="contact-link">
+                    <div class="contact-title">
+                        <span class="contact-icon"></span>
+                             <p class="contact-text">
+                                <span class="contact-text1">お問い合わせ</span>
+                                <span class="contact-text1">面談予約</span>
+                            </p>
+                     </div>
+                 </a>
+           </div>
        </nav>
     </header>
--- wp-content/themes/studio-base/index.php
+++ wp-content/themes/studio-base/index.php
@@ -8,36 +8,36 @@
             </div>
             <div class="NewsContent">
                 <div class="news">
-                    <a href="#">
-                    <dl class="news-area">
-                            <dt class="news-day">2022.05.01</dt>
-                            <dt class="news-title">新しいメニューができました。</dt>
-                        </a>
-                    <dl>
+                    <a href="">
+                        <dl class="news-area">
+                                <dt class="news-day">2022.05.01</dt>
+                                <dt class="news-title">新しいメニューができました。</dt>
+                        <dl>
+                    </a>
                 </div>
                 <div class="news">
                     <a href="#">
-                    <dl class="news-area">
-                            <dt class="news-day">2022.4.10</dt>
-                            <dt class="news-title">花見に行きました。</dt>
-                        </a>
-                    <dl>
+                        <dl class="news-area">
+                                <dt class="news-day">2022.4.10</dt>
+                                <dt class="news-title">花見に行きました。</dt>
+                        <dl>
+                    </a>
                 </div>
                 <div class="news">
                     <a href="#">
-                    <dl class="news-area">
-                            <dt class="news-day">2022.1.01</dt>
-                            <dt class="news-title">2022年もよろしくお願いします。</dt>
-                        </a>
-                    <dl>
+                        <dl class="news-area">
+                                <dt class="news-day">2022.1.01</dt>
+                                <dt class="news-title">2022年もよろしくお願いします。</dt>
+                        <dl>
+                    </a>
                 </div>
                 <div class="news">
                     <a href="#">
-                    <dl class="news-area">
-                            <dt class="news-day">2022.02.10</dt>
-                            <dt class="news-title">節分大会</dt>
-                        </a>
-                    <dl>
+                        <dl class="news-area">
+                                <dt class="news-day">2022.02.10</dt>
+                                <dt class="news-title">節分大会</dt>
+                        <dl>
+                    </a>
                 </div>
            </div>
       </div>
