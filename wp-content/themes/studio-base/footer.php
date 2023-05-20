6a142dc97d56dfe821ab2bf9e5767399e9dedf22
yamamura <ayatsujisan2@gmail.com>
2023-05-20 13:24:26 +0900

    コミットします

--- wp-content/themes/studio-base/footer.php
+++ wp-content/themes/studio-base/footer.php
@@ -1,5 +1,47 @@
 <footer>
-    <p>footerです</p>
+    <div class="footer">
+        <div class="footer-area">
+            <div class="footer-subarea">
+                <a href="#" class="logo"><img src="<?php echo get_template_directory_uri(); ?>/images/onigiri_tenmusu.png" alt="ヘッダーロゴ" class="logo-img-footer"></a>
+            </div>
+        </div>
+        <div class="footer-text">
+            <p>静岡県浜松市中区住吉町123-1メゾンプレール103</p>
+            <p>電話番号: 020-1234-567</p>
+        </div>
+    </div>
+    <div class="footer-menu">
+        <div class="footer-menu-detail">
+            <ul class="footer-menu-list">
+                <li class="footer-menu-text">studio baseとは</li>
+                <li class="footer-menu-text">メニュー</li>
+                <li class="footer-menu-text">新着情報</li>
+                <li class="footer-menu-text">施工事例</li>
+                <li class="footer-menu-text">完成までの流れ</li>
+                <li class="footer-menu-text">お問い合わせ・面談予約</li>
+            </ul>
+        </div>
+    </div>
+    <div class="footer-sitemap">
+        <div class="footer-sitemap-detail">
+            <div class="sitemap-sns">
+                <ul class="sns-area-footer">
+                    <li class="sns-area-detail"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/insta.png" alt="インスタアイコン" alt="" class="sns-icon-footer"></a></li>
+                    <li lass="sns-area-detail"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/twitter.png" alt="twitterアイコン" alt="" class="sns-icon-footer"></a></li>
+                </ul>
+            </div>
+            <div class="footer-copylight">
+                <ul class="copylight-menu">
+                    <li class="cp-menu-list">
+                        <a href="" class="copymenu">プライバシーポリシー</a>
+                    </li>
+                    <li class="cp-menu-list">
+                        <a href="" class="copymenu">サイトマップ</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </footer>
     
 </body>
